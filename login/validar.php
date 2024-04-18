e068dbae6385206b7c0b19fd86af5f02157fbfe9
cristian033 <cristianarielmoreno2006@gmail.com>
2024-04-17 21:45:36 -0300

    front del login terminado

---
+++ login/validar.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+$nombre = pepe ;
+$password = 1234 ;
+ 
+ if(isset('ingresar'))
+        echo"hacer la validacion";
+   else
+        echo"eliminar session"; 
+ 
+
+
+?>
